f20a96d979817575c37f0b3cf920bda1036e2a56
Fernando Rocha Olivera <frochaolivera18@hotmail.com>
2021-01-07 21:02:05 -0500

    Ejemplo de llamada a una API con callback

--- src/callback/challenge.js
+++ src/callback/challenge.js
@@ -1,4 +1,5 @@
-let XMLHttpRequest = required('xmlhttprequest').XMLHttpRequest;
+let XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+let API = 'https://rickandmortyapi.com/api/character/';
 
 function fetchData(url_api, callback){
     let xhttp = new XMLHttpRequest(); //-- Instanciado de objeto
@@ -16,3 +17,17 @@ function fetchData(url_api, callback){
     }
     xhttp.send();
 }
+
+fetchData(API, function(error1, data1) {
+    if (error1) return console.error(error1);
+    fetchData(API + data1.results[0].id, function(error2, data2){
+        if (error2) return console.error(error2);
+        fetchData(data2.origin.url, function(error3, data3) {
+            if (error3) return console.error(error3);
+            console.log(data1.info.count);
+            console.log(data1.info.pages);
+            console.log(data2.origin);
+            console.log(data3.dimension);
+        })
+    })
+});
